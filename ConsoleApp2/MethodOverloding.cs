d677115dbf4d993ceb6c26595ba2c806ed6be5de
Ayush Jindal <jindalayush42@gmail.com>
2025-12-20 23:24:43 +0530

    method overloding

---
+++ ConsoleApp2/MethodOverloding.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class MethodOverloding
+    {
+        public void test()
+        {
+            Console.WriteLine("1st Method");
+        }
+        public void test(int i)
+        {
+            Console.WriteLine("2nd Method");
+        }
+
+        public void test(string s)
+        {
+            Console.WriteLine("3rd Method");
+        }
+        public void test(string s , int i)
+        {
+            Console.WriteLine("4th Method");
+
+        }
+        public void test(int i ,string s)
+        {
+            Console.WriteLine("5th Method");
+        }
+        static void Main(string [] args)
+        {
+            MethodOverloding p = new MethodOverloding();
+            p.test(10);
+            //p.test2(10);
+            //p.test3("ayush")
+        }
+    }
+}
