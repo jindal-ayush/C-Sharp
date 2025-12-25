c49bd4f90665cac1521c7938157c2cf0ce22d34c
Ayush Jindal <jindalayush42@gmail.com>
2025-12-25 23:31:50 +0530

    method hiding

---
+++ ConsoleApp2/Polymorphism.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Polymorphism
+    {
+        public virtual void  test1()
+        {
+            Console.WriteLine("parent class method test 1");
+        }
+        public void test2()
+        {
+            Console.WriteLine("parent class method test 2");
+        }
+
+    }
+
+    class LoadParent : Polymorphism
+    {
+        public override void test1()
+        {
+            Console.WriteLine("child class method test 1");
+        }
+        public new void Test2()
+        {
+            Console.WriteLine("child class method test 2");
+        }
+        static void Main()
+        {
+            LoadParent c = new LoadParent();
+            c.test1();
+            c.test2();
+            //Console.WriteLine();
+        }
+    }
+
+}
